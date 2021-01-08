ce6a439d9ea612c25ecc0122f005503dca24460b
wiktorderen <wiktor1474@wp.pl>
2021-01-08 17:10:19 +0100

    lab09
    lab09

---
+++ Assets/Scripts/GameOverCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverCollider: MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GAME OVER !!!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GAME OVER !!!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
